174141d0e1b05c8016c1013a0b3c9c8fe936693b
Artem Shvadskiy <shpen72991@gmail.com>
2015-07-26 09:00:41 -0700

    Revert to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
+    address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'heroku.com',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    authentication:       :plain,
+    domain:               ENV["GMAIL_DOMAIN"],
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
     enable_starttls_auto: true  }
 end
